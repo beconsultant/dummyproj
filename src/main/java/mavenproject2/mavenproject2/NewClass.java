c13b5339e3b70d502b1ed1f3af3141c1a147ff20
marogase <s.rodriguez.garcia@ibermatica.com>
2016-11-07 19:45:51 +0100

    commitone

---
+++ src/main/java/mavenproject2/mavenproject2/NewClass.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mavenproject2.mavenproject2;
+
+/**
+ *
+ * @author marogase
+ */
+public class NewClass {
+    
+    
+    public static void main(String[] args) {
+    System.out.println("Soy la clase del proyecto tonto");
+    }
+}
